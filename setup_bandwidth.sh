23eab51da88f57fc0253696a8ebe4dc6092c17de
ukkvkkv <109349731+ukkvkkv@users.noreply.github.com>
2026-04-20 11:32:37 +0500

    setup_bandwidth.sh

--- setup_bandwidth.sh
+++ setup_bandwidth.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-sed -i 's/200 mbps/500 mbps/g; s/200 Mbps/500 Mbps/g; s/200mbps/500mbps/g; s/200Mbps/500Mbps/g' /etc/hysteria/config.json
+sed -i '
+s/200 mbps/0 mbps/g;
+s/200 Mbps/0 Mbps/g;
+s/200mbps/0mbps/g;
+s/200Mbps/0Mbps/g;
+s/"ignoreClientBandwidth":[[:space:]]*false/"ignoreClientBandwidth": true/g
+' /etc/hysteria/config.json
 
 systemctl restart hysteria-server
 
